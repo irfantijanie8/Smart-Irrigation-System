e3a92f719fd72341a5d13710a021cfbbf6e9dbe6
unknown <irfantijanie6@gmail.com>
2024-01-28 10:51:58 +0800

    first

---
+++ esp32/esp32.ino
@@ -0,0 +1,159 @@
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";
+const char* mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+//Used Pins
+const int DHT11_PIN = 39;      
+const int RELAY_PIN = 38;
+const int RAIN_PIN = 48;
+const int MOIST_PIN = A1;
+const int RAIN_POWER = 47;
+
+float temperature = 0;
+float humidity = 0;
+
+//input sensor
+#define DHTTYPE DHT11
+DHT dht(DHT11_PIN, DHTTYPE);
+
+//define pump state
+bool pump_state = false;
+
+void setup() {
+  Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(RAIN_PIN, INPUT);
+  pinMode(MOIST_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(RAIN_POWER, OUTPUT);
+  digitalWrite(RAIN_POWER, LOW);
+  pump_state = false;
+  dht.begin();
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/pump") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(RELAY_PIN, HIGH);
+      pump_state = true;
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(RELAY_PIN, LOW);
+      pump_state = false;
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/pump");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+
+    //read telemetry data
+    float h = dht.readHumidity();
+    int t = dht.readTemperature();
+
+    //read moist telemetry data
+    int moist_analog = analogRead(MOIST_PIN);
+    int _moisture = ( 100 - ( (moist_analog/4095.00) * 100 ) );
+
+    JSONVar payloadObject;    
+    payloadObject["Humidity"] = h;
+    payloadObject["Temperature"] = t;
+    payloadObject["Soil moisture"] = _moisture;
+    payload = JSON.stringify(payloadObject);  
+    client.publish("esp32/sensor", payload);
+
+    bool rain_read = is_raining();
+    if(rain_read & pump_state)
+      client.publish("esp32/pump", "off");
+    delay(5000);
+  }
+}
+
+bool is_raining(){
+  digitalWrite(RAIN_POWER, HIGH);
+  delay(10);
+  bool val = digitalRead(RAIN_PIN);
+  digitalWrite(RAIN_POWER, LOW);
+  return !val;
+}
---
+++ rain_prediction.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import joblib
+import time
+import json
+import requests
+
+# Load the pre-trained model and preprocessing tools
+best_pipeline = joblib.load('path_to_saved_model/best_pipeline.joblib')
+scaler = joblib.load('path_to_saved_model/scaler.joblib')
+selector = joblib.load('path_to_saved_motdel/selector.joblib')
+
+#mqtt
+mqtt_broker_address = 'localhost'
+mqtt_topic = 'esp32/sensor'
+
+
+
+def on_message(client, userdata, message):
+    payload = message.payload.decode('utf-8')
+    print(f'Received message: {payload}')
+
+    sensor = json.loads(payload)
+    avgtemp = sensor["Temperature"]
+    avghumidity = sensor["Humidity"]
+    moisture = sensor["Soil Moisture"]
+    forecast = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+    avgwindspeed = forecast["current"]["wind_speed_10m"]
+
+    if avgtemp is not None and avghumidity is not None and avgwindspeed is not None and moisture < 20:
+        # Preprocess the sensor data
+        preprocessed_data = preprocess_sensor_data(avgtemp, avghumidity, avgwindspeed)
+
+        # Make a prediction
+        prediction = make_prediction(preprocessed_data)
+
+        # Output the prediction
+        rain_prediction = "Yes" if prediction[0] else "No"
+        print(f"Prediction: Will it rain tomorrow? {rain_prediction}")
+
+
+    # Wait for some time before reading new data
+    time.sleep(60)  # for example, wait for 60 seconds
+
+
+# Function to read sensor data from a file
+def read_sensor_data(file_path):
+    try:
+        # Assuming the file has one line with comma-separated values for avgtemp, avghumidity, avgwindspeed
+        with open(file_path, 'r') as file:
+            line = file.readline()
+            avgtemp, avghumidity, avgwindspeed = map(float, line.strip().split(','))
+        return avgtemp, avghumidity, avgwindspeed
+    except Exception as e:
+        print(f"Error reading sensor data: {e}")
+        return None, None, None
+
+# Function to preprocess sensor data
+def preprocess_sensor_data(avgtemp, avghumidity, avgwindspeed):
+     # Create a DataFrame with the sensor data
+    sensor_data = pd.DataFrame({
+        'avgtemp': [avgtemp],
+        'avghumidity': [avghumidity],
+        'avgwindspeed': [avgwindspeed]
+    })
+    # Apply the same preprocessing as during training
+    sensor_data_scaled = scaler.transform(sensor_data)
+    sensor_data_selected = selector.transform(sensor_data_scaled)
+
+    return sensor_data_selected
+
+# Function to make predictions
+def make_prediction(preprocessed_data):
+   # Use the loaded model to make predictions
+    prediction = best_pipeline.predict(preprocessed_data)
+    return prediction
+
+# Path to the file containing sensor data
+sensor_data_file_path = 'path_to_sensor_data_file.txt'
+
+client = mqtt.Client()
+client.on_message = on_message
+# Connect to MQTT broker
+client.connect(mqtt_broker_address, 1883, 60)
+# Subscribe to MQTT topic
+client.subscribe(mqtt_topic)
+# Start the MQTT loop
+client.loop_forever()
